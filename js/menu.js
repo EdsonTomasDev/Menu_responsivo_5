d7fd666190c0c981ce74f99890367c881a93755a
Tomas Edson <t.edson.almeida@hotmail.com>
2022-02-26 17:06:43 -0300

    config menu mobile em X

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,22 @@ function ativar_menu(){
     const obj = document.querySelector("#lista_menu");
     const classes = obj.classList;
 
-    //alert(classes);
+    // alert(classes);
 
     //Adiciona a classe que faz aparecer a ul
     classes.toggle('menu_lista-ativa');
+    
+    //Percorre as 3 barras e adiciona uma classe para cada
+    for(i=1; i <= 3; i++){
 
-
+        let id_nome = "linha" + i;
+    
+        
+        const obj_barras = document.querySelector("#" + id_nome);
+        const classe_barras = obj_barras.classList;
+        //Adiciona a classe que modificará a posição de cada barra
+        classe_barras.toggle('mobile_menu_line' + i);
+    }
 
 }
 
